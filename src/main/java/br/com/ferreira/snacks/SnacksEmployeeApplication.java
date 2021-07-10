b8f283758405230d458c82561837d05049fa5542
fehferreira <felipe.ferreira1003@gmail.com>
2021-07-10 20:32:35 -0300

    Starting of project on Snacks Employee

---
+++ src/main/java/br/com/ferreira/snacks/SnacksEmployeeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferreira.snacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnacksEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnacksEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ferreira/snacks/SnacksEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferreira.snacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnacksEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
