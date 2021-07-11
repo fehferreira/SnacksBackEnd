abd23d2c287d2140f5345b8ad1901c31d00ac133
fehferreira <felipe.ferreira1003@gmail.com>
2021-07-10 22:18:36 -0300

    Develop method on serviceLayer with TDD

--- src/main/java/br/com/ferreira/snacks/service/EmployeeService.java
+++ src/main/java/br/com/ferreira/snacks/service/EmployeeService.java
@@ -1,10 +1,15 @@
 package br.com.ferreira.snacks.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ferreira.snacks.controller.form.EmployeeForm;
+import br.com.ferreira.snacks.exception.EntityPresentException;
 import br.com.ferreira.snacks.model.Employee;
 import br.com.ferreira.snacks.repository.EmployeeRepository;
 
@@ -17,5 +22,28 @@ public class EmployeeService {
 	public List<Employee> findAllEmployees(){
 		return repository.findAll(); 
 	}
+	
+	public Employee findEmployeeDetails(Long idEmployee) {
+		Optional<Employee> optional = repository.findById(idEmployee);
+		if(optional.isPresent())
+			return optional.get();
+		throw new EntityNotFoundException();
+	}
+	
+	public Employee createEmployee(EmployeeForm form) {
+		Employee createEmployee = form.getEmployee();
+	
+		Optional<Employee> optional = repository.findByName(createEmployee.getName());
+		if(optional.isPresent()) {
+			throw new EntityPresentException();
+		}
+		
+		return repository.save(createEmployee);
+	}
+	
+	
+	
+	
+	
 	
 }
--- src/test/java/br/com/ferreira/snacks/service/EmployeeServiceTest.java
+++ src/test/java/br/com/ferreira/snacks/service/EmployeeServiceTest.java
@@ -1,20 +1,28 @@
 package br.com.ferreira.snacks.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import br.com.ferreira.snacks.controller.form.EmployeeForm;
+import br.com.ferreira.snacks.exception.EntityPresentException;
 import br.com.ferreira.snacks.model.Employee;
 import br.com.ferreira.snacks.repository.EmployeeRepository;
 
@@ -52,4 +60,64 @@ class EmployeeServiceTest {
 		
 		assertThat(returnedList).isEqualToIgnoringWhitespace(listToCompare);
 	}
+	
+	
+	@Test
+	void findEmployeeDetails_shouldReturnAEmployee() {
+		Employee employee = this.listEmployees.get(0);
+		
+		when(repositoryMock.findById(employee.getId())).thenReturn(Optional.of(employee));
+		
+		Employee returnedEmployee = service.findEmployeeDetails(employee.getId());
+	
+		assertEquals(returnedEmployee.getId(), employee.getId());
+		assertEquals(returnedEmployee.getName(), employee.getName());
+	}
+	
+	@Test
+	void findEmployeeDetails_shouldThrowException_EntityNotFound() {
+		when(repositoryMock.findById(any())).thenReturn(Optional.empty());
+		
+		try {
+			service.findEmployeeDetails(1L);
+		}catch (RuntimeException exception) {
+			assertThat(exception.getClass()).isEqualTo(EntityNotFoundException.class);
+		}
+		
+		Mockito.verify(repositoryMock).findById(1L);
+	}
+	
+	@Test
+	void createEmployee_shouldReturnEmployeeCreated() {
+		EmployeeForm form = new EmployeeForm("Felipe Ferreira", false, false);
+		Employee createdEmployee = form.getEmployee();
+		createdEmployee.setId(1L);
+	
+		when(repositoryMock.findByName(createdEmployee.getName())).thenReturn(Optional.empty());
+		when(repositoryMock.save(any())).thenReturn(createdEmployee);
+		
+		Employee returnedEmployee = service.createEmployee(form);
+		
+		assertEquals(returnedEmployee.getId(), createdEmployee.getId());
+		assertEquals(returnedEmployee.getName(), createdEmployee.getName());
+	}
+	
+	@Test
+	void createEmployee_shouldThrowException_EmployeeExist() {
+		EmployeeForm form = new EmployeeForm("Felipe Ferreira", false, false);
+		Employee createdEmployee = form.getEmployee();
+		createdEmployee.setId(1L);
+	
+		when(repositoryMock.findByName(createdEmployee.getName()))
+			.thenReturn(Optional.of(createdEmployee));
+		
+		try {
+			service.createEmployee(form);
+		}catch (Exception exception) {
+			assertThat(exception.getClass()).isEqualTo(EntityPresentException.class);
+		}
+		
+		Mockito.verify(repositoryMock).findByName(createdEmployee.getName());
+	}
+	
 }
