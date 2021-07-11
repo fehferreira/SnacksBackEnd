dfa3357c16664d605fc40ef11fa9c7b372ddab26
fehferreira <felipe.ferreira1003@gmail.com>
2021-07-11 02:19:52 -0300

    Create delete and update method on EmployeeController

--- src/main/java/br/com/ferreira/snacks/controller/EmployeeController.java
+++ src/main/java/br/com/ferreira/snacks/controller/EmployeeController.java
@@ -6,10 +6,14 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ferreira.snacks.controller.form.EmployeeForm;
@@ -33,4 +37,19 @@ public class EmployeeController {
 	public Employee createEmployee(@RequestBody @Valid EmployeeForm form) {
 		return service.createEmployee(form);
 	}
+	
+	@DeleteMapping
+	@GetMapping("/${id}")
+	public Employee deleteEmployee(@PathVariable Long id) {
+		return service.deleteEmployee(id);
+	}
+	
+	@PutMapping
+	public Employee updateEmployee(@RequestBody @Valid EmployeeForm updateForm) {
+		return service.updateEmployee(updateForm);
+	}
+	
 }
+
+
+
