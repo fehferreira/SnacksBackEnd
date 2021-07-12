21fc56846b1c14b146f5e5a870f693b4649aa3ca
fehferreira <felipe.ferreira1003@gmail.com>
2021-07-12 10:37:05 -0300

    Develop SnacksWork entity and create a relational bound with Employee entity

--- src/main/java/br/com/ferreira/snacks/model/SnacksWork.java
+++ src/main/java/br/com/ferreira/snacks/model/SnacksWork.java
@@ -1,14 +1,12 @@
 package br.com.ferreira.snacks.model;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,28 +14,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "employee")
+@Table(name = "work")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Employee {
+public class SnacksWork {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long idWork;
 	
-	@NotNull(message = "This value cant be NULL")
-	private String name;
+	private LocalDateTime dateStartWork;
 	
-	private boolean isWorking = false;
+	private Long idWorkingEmployee;
 	
-	private boolean isAusent = false;
-	
-	private Long previousEmployeeId;
-	
-	private Long nextEmployeeId;
-	
-	@OneToMany
-	private List<SnacksWork> realizedWorks;
 }
